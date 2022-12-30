e685ca0891d2c14d2c879d48d6b54569b31ff909
kruumy <joshuampauls@gmail.com>
2022-12-29 20:54:24 -0500

    removed unneeded debug lines

--- Toree3D/InfiniteJumps/Main.cs
+++ Toree3D/InfiniteJumps/Main.cs
@@ -11,16 +11,8 @@ namespace InfiniteJumps
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             playerSystem = UnityEngine.Object.FindObjectOfType<PlayerSystem>();
-            if (playerSystem != null)
-                MelonLogger.Msg("Found PlayerSystem Object");
-
             doublejump = typeof(PlayerSystem).GetField("doublejump", BindingFlags.NonPublic | BindingFlags.Instance);
-            if (doublejump != null)
-                MelonLogger.Msg("Found doublejump Field");
-
             donejumps = typeof(PlayerSystem).GetField("donejumps", BindingFlags.NonPublic | BindingFlags.Instance);
-            if (donejumps != null)
-                MelonLogger.Msg("Found donejumps Field");
         }
         public override void OnLateUpdate()
         {
