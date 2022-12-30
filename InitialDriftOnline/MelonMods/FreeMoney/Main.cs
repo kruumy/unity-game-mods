a93d407655c859ca789059b0fbc9609d59a1618e
kruumy <joshuampauls@gmail.com>
2022-12-30 16:32:41 -0500

    added free money and started on fake level

---
+++ InitialDriftOnline/MelonMods/FreeMoney/Main.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace FreeMoney
+{
+    public class Main : MelonMod
+    {
+        SRAdminTools AdminTools => UnityEngine.Object.FindObjectOfType<SRAdminTools>();
+        public override void OnLateUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F9))
+            {
+                if (AdminTools != null)
+                {
+                    AdminTools.GiveMoney();
+                    MelonLogger.Msg("Given $1000");
+                }
+                else
+                {
+                    MelonLogger.Error("AdminTools == null");
+                }
+            }
+        }
+    }
+}
