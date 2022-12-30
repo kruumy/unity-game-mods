6ad281e0445b2998733b5afa5110c27bd45287dd
kruumy <joshuampauls@gmail.com>
2022-12-30 13:19:29 -0500

    added ido fov modifier

---
+++ InitialDriftOnline/MelonMods/FOVModifier/Camera.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+
+namespace FOVModifier
+{
+    public static class Camera
+    {
+        public static float? FieldOfView
+        {
+            get
+            {
+                if (TPSMinimumFOV != null)
+                    return TPSMinimumFOV;
+                else if (targetFieldOfView != null)
+                    return targetFieldOfView - 15.0f;
+                else if (TPSMaximumFOV != null)
+                    return TPSMaximumFOV - 30.0f;
+                else
+                    return null;
+            }
+            set
+            {
+                TPSMinimumFOV = value;
+                targetFieldOfView = value + 15.0f;
+                TPSMaximumFOV = value + 30.0f;
+            }
+        }
+        private static float? TPSMinimumFOV
+        {
+            get => RCC_SceneManager.Instance.activePlayerCamera.TPSMinimumFOV;
+            set => RCC_SceneManager.Instance.activePlayerCamera.TPSMinimumFOV = (float)value;
+        }
+        private static float? TPSMaximumFOV
+        {
+            get => RCC_SceneManager.Instance.activePlayerCamera.TPSMaximumFOV;
+            set => RCC_SceneManager.Instance.activePlayerCamera.TPSMaximumFOV = (float)value;
+        }
+        private static float? targetFieldOfView
+        {
+            get
+            {
+                FieldInfo fieldInfo = typeof(RCC_Camera).GetField("targetFieldOfView", BindingFlags.NonPublic | BindingFlags.Instance);
+                return (float)fieldInfo.GetValue(RCC_SceneManager.Instance.activePlayerCamera);
+            }
+            set
+            {
+                FieldInfo fieldInfo = typeof(RCC_Camera).GetField("targetFieldOfView", BindingFlags.NonPublic | BindingFlags.Instance);
+                fieldInfo.SetValue(RCC_SceneManager.Instance.activePlayerCamera, value);
+            }
+        }
+    }
+}
---
+++ InitialDriftOnline/MelonMods/FOVModifier/Main.cs
@@ -0,0 +1,28 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace FOVModifier
+{
+    public class Main : MelonMod
+    {
+        public override void OnLateUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Minus))
+            {
+                if (Camera.FieldOfView != null)
+                {
+                    Camera.FieldOfView -= 5;
+                    MelonLogger.Msg($"{nameof(Camera.FieldOfView)} = {Camera.FieldOfView}");
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.Equals))
+            {
+                if (Camera.FieldOfView != null)
+                {
+                    Camera.FieldOfView += 5;
+                    MelonLogger.Msg($"{nameof(Camera.FieldOfView)} = {Camera.FieldOfView}");
+                }
+            }
+        }
+    }
+}
