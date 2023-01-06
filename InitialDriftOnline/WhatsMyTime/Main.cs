40a009ede4f20a8363a1e3b51092d6a2f47d1d34
kruumy <joshuampauls@gmail.com>
2023-01-05 22:14:38 -0500

    added whatsmytime mod

---
+++ InitialDriftOnline/WhatsMyTime/Main.cs
@@ -0,0 +1,45 @@
+﻿using MelonLoader;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WhatsMyTime
+{
+    public class Main : MelonMod
+    {
+        private readonly Rect LabelRect = new Rect((Screen.width / 2) - 65, Screen.height - 35, 0, 0);
+
+        private readonly GUIStyle LabelStyle = new GUIStyle()
+        {
+            fontSize = 35,
+            fontStyle = FontStyle.BoldAndItalic,
+            normal = new GUIStyleState() { textColor = Color.white },
+            wordWrap = false
+        };
+
+        public override void OnInitializeMelon()
+        {
+            SRToffuManagerWrapper.Initialize();
+            SRToffuManagerWrapper.OnTofuRunStart += OnTofuRunStart;
+            SRToffuManagerWrapper.OnTofuRunStopped += OnTofuRunStopped; ;
+        }
+
+        private void GuiWork()
+        {
+            string elapsedTime = string.Format("{0:000}.{1:000}",
+            SRToffuManagerWrapper.Timer.Elapsed.TotalSeconds,
+            SRToffuManagerWrapper.Timer.Elapsed.Milliseconds);
+            GUI.Label(LabelRect, elapsedTime, LabelStyle);
+        }
+
+        private void OnTofuRunStart(object sender, System.EventArgs e)
+        {
+            MelonEvents.OnGUI.Subscribe(GuiWork);
+        }
+
+        private async void OnTofuRunStopped(object sender, System.EventArgs e)
+        {
+            await Task.Delay(10000);
+            MelonEvents.OnGUI.Unsubscribe(GuiWork);
+        }
+    }
+}
---
+++ InitialDriftOnline/WhatsMyTime/SRToffuManagerWrapper.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using System;
+using System.Diagnostics;
+using UnityEngine.SceneManagement;
+
+namespace WhatsMyTime
+{
+    public static class SRToffuManagerWrapper
+    {
+        public static event EventHandler OnTofuRunStart;
+
+        public static event EventHandler OnTofuRunStopped;
+
+        public static SRToffuManager SRToffuManager => UnityEngine.Object.FindObjectOfType<SRToffuManager>();
+
+        public static Stopwatch Timer { get; } = new Stopwatch();
+
+        public static void Initialize()
+        {
+            SceneManager.activeSceneChanged += activeSceneChanged;
+        }
+
+        private static void activeSceneChanged(Scene arg0, Scene arg1)
+        {
+            OnTofuRunStopped.Invoke(null, EventArgs.Empty);
+        }
+
+        [HarmonyPatch(typeof(SRToffuManager), "FinDeLivraison")]
+        private static class FinDeLivraisonPatch
+        {
+            private static void Postfix(SRToffuManager __instance)
+            {
+                if (__instance == SRToffuManager)
+                {
+                    OnTofuRunStopped.Invoke(__instance, EventArgs.Empty);
+                    Timer.Stop();
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(SRToffuManager), "StartCompteur")]
+        private static class StartCompteurPatch
+        {
+            private static void Postfix(SRToffuManager __instance)
+            {
+                if (__instance == SRToffuManager)
+                {
+                    OnTofuRunStart.Invoke(__instance, EventArgs.Empty);
+                    Timer.Restart();
+                }
+            }
+        }
+    }
+}
