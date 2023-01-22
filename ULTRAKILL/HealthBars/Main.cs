b41bff0255e6a8fb6206158b94187c3256e36a9f
kruumy <joshuampauls@gmail.com>
2023-01-22 15:28:49 -0500

    healthbars now scale to enemy distance

--- ULTRAKILL/HealthBars/Main.cs
+++ ULTRAKILL/HealthBars/Main.cs
@@ -7,34 +7,43 @@ namespace HealthBars
 {
     public class Main : MelonMod
     {
-        public static Dictionary<EnemyType, float> MaxHealth = new Dictionary<EnemyType, float>();
-        public static Texture2D RedTexture = MakeTex(2, 2, Color.red);
-        public static Texture2D YellowTexture = MakeTex(2, 2, Color.yellow);
-        public static Texture2D GreenTexture = MakeTex(2, 2, Color.green);
-        public static GUIStyle RedStyle = new GUIStyle()
+        public readonly static Dictionary<EnemyType, float> MaxHealth = new Dictionary<EnemyType, float>();
+        public readonly static Texture2D RedTexture = MakeTex(1, 1, Color.red);
+        public readonly static Texture2D YellowTexture = MakeTex(1, 1, Color.yellow);
+        public readonly static Texture2D GreenTexture = MakeTex(1, 1, Color.green);
+        public readonly static Texture2D BlackTexture = MakeTex(1, 1, Color.black);
+        public readonly static GUIStyle RedStyle = new GUIStyle()
         {
             normal =
             {
                 background = RedTexture,
                 textColor = Color.black
             },
+            richText = false,
             fontSize = 14,
             fontStyle = FontStyle.Italic,
             wordWrap = false,
             alignment = TextAnchor.MiddleCenter
         };
-        public static GUIStyle YellowStyle = new GUIStyle(RedStyle)
+        public readonly static GUIStyle YellowStyle = new GUIStyle(RedStyle)
         {
             normal =
             {
                 background = YellowTexture,
             },
         };
-        public static GUIStyle GreenStyle = new GUIStyle(RedStyle)
+        public readonly static GUIStyle GreenStyle = new GUIStyle(RedStyle)
         {
             normal =
             {
                 background = GreenTexture,
+            },
+        };
+        public readonly static GUIStyle BlackStyle = new GUIStyle(RedStyle)
+        {
+            normal =
+            {
+                background = BlackTexture,
             },
         };
 
@@ -44,18 +53,24 @@ namespace HealthBars
             {
                 if (!enemy.dead)
                 {
-                    Vector3 screenPos = Camera.main.WorldToScreenPoint(enemy.gameObject.transform.position);
-                    screenPos.y = Screen.height - screenPos.y;
-                    if (screenPos.z > 0)
+                    Vector3 enemyPosOnScreen = Camera.main.WorldToScreenPoint(enemy.gameObject.transform.position);
+                    enemyPosOnScreen.y = Screen.height - enemyPosOnScreen.y;
+                    if (enemyPosOnScreen.z > 0)
                     {
-                        int width = 75;
+                        float globalScale = 20.0f;
+                        int width = (int)Mathf.Clamp(75 * globalScale / enemyPosOnScreen.z, 0, 100);
+                        int height = (int)Mathf.Clamp(15 * globalScale / enemyPosOnScreen.z, 0, 20);
+                        int fontSize = (int)Mathf.Clamp(14 * globalScale / enemyPosOnScreen.z, 0, 18);
+                        RedStyle.fontSize = fontSize;
+                        YellowStyle.fontSize = fontSize;
+                        GreenStyle.fontSize = fontSize;
                         int healthWidth = (int)Map(enemy.health, 0, MaxHealth[enemy.enemyType], 0, width);
-                        int height = 15;
                         int healthPercentage = ((int)(healthWidth * (100.0f / width)));
-                        GUI.Box(new Rect(screenPos.x - width / 2, screenPos.y - height / 2, width, height), string.Empty);
-                        GUIStyle healthStyle;
-                        healthStyle = healthPercentage > 80 ? GreenStyle : healthPercentage > 40 ? YellowStyle : RedStyle;
-                        GUI.Box(new Rect(screenPos.x - width / 2, screenPos.y - height / 2, healthWidth, height), healthPercentage.ToString() + "%", healthStyle);
+                        enemyPosOnScreen.x -= width / 2;
+                        enemyPosOnScreen.y -= height / 2;
+                        GUI.Box(new Rect(enemyPosOnScreen.x, enemyPosOnScreen.y, width, height), GUIContent.none, BlackStyle);
+                        GUIStyle healthStyle = healthPercentage > 80 ? GreenStyle : healthPercentage > 40 ? YellowStyle : RedStyle;
+                        GUI.Box(new Rect(enemyPosOnScreen.x, enemyPosOnScreen.y, healthWidth, height), healthPercentage.ToString() + "%", healthStyle);
                     }
                 }
             }
