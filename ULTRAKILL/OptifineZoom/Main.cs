2061782c6a9f0ab02f543d34c7a3d44a56ab42c4
kruumy <joshuampauls@gmail.com>
2023-01-21 21:23:14 -0500

    added ultrakill mod

---
+++ ULTRAKILL/OptifineZoom/Main.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace OptifineZoom
+{
+    public class Main : MelonMod
+    {
+        public float FROM_FOV { get; set; }
+        public float TO_FOV => FROM_FOV / 4;
+        public override void OnLateUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                FROM_FOV = CameraController.Instance.defaultFov;
+                CameraController.Instance.defaultFov = TO_FOV;
+            }
+            else if (Input.GetKeyUp(KeyCode.C))
+            {
+                CameraController.Instance.defaultFov = FROM_FOV;
+            }
+
+
+            EnemyTracker.Instance.enemies[0].transform.po
+        }
+    }
+}
---
+++ ULTRAKILL/OptifineZoom/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("InfiniteDashes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InfiniteDashes")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: MelonInfo(typeof(OptifineZoom.Main), "OptifineZoom", "1.0.0", "kruumy")]
+[assembly: MelonGame("Hakita", "ULTRAKILL")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("263177f9-1093-4c5b-96cc-9bc829e7b172")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
