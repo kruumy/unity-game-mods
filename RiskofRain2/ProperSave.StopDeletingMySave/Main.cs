066eac35cb6210e422f201e738f80752bc81aece
kruumy <joshuampauls@gmail.com>
2023-02-04 15:34:19 -0500

    added stopdeletingmysave to ror2

---
+++ RiskofRain2/ProperSave.StopDeletingMySave/Main.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Linq;
+using System.Reflection;
+
+namespace ProperSave.StopDeletingMySave
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency("com.KingEnderBrine.ProperSave")]
+    public class Main : BaseUnityPlugin
+    {
+        public const string PluginAuthor = "kruumy";
+        public const string PluginName = "StopDeletingMySave";
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginVersion = "1.0.0";
+
+        public void Awake()
+        {
+            MethodInfo RunOnServerGameOver = Assembly
+                .Load("ProperSave")
+                .GetTypes()
+                .First(t => t.Name == "Saving")
+                .GetMethod("RunOnServerGameOver", BindingFlags.Static | BindingFlags.NonPublic);
+            Logger.LogInfo("Found Target " + RunOnServerGameOver.Name);
+            Harmony harmony = new Harmony(PluginGUID);
+            HarmonyMethod harmonyMethod = new HarmonyMethod(typeof(Main), "RunOnServerGameOver_Prefix");
+            harmonyMethod.method = typeof(Main).GetMethod("RunOnServerGameOver_Prefix", BindingFlags.Instance | BindingFlags.NonPublic);
+            Logger.LogInfo("Created Prefix " + harmonyMethod.method.Name);
+            harmony.Patch(RunOnServerGameOver, harmonyMethod);
+            Logger.LogInfo("Patched ProperSave.Saving.RunOnServerGameOver Successfully!");
+        }
+
+        private bool RunOnServerGameOver_Prefix()
+        {
+            Logger.LogInfo("Preventing Execution of ProperSave.Saving.RunOnServerGameOver");
+            return false;
+        }
+    }
+}
