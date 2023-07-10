4dfd2d9e87a54e9540e00c9247f00b19d96d0ea9
kruumy <joshuampauls@gmail.com>
2023-07-10 13:55:30 -0400

    added new mod

---
+++ 69 Balls/NoMotionBlur/Melon.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using System.Timers;
+
+namespace NoMotionBlur
+{
+    public class Melon : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            Timer Timer = new Timer(1000);
+            Timer.Elapsed += Timer_Elapsed;
+            Timer.Start();
+            MelonLogger.Msg("Started NoMotionBlur Timer");
+        }
+
+        private void Timer_Elapsed( object sender, ElapsedEventArgs e )
+        {
+            try
+            {
+                foreach ( UnityEngine.Rendering.Universal.MotionBlur item in UnityEngine.Object.FindObjectsOfTypeAll(typeof(UnityEngine.Rendering.Universal.MotionBlur)) )
+                {
+                    if ( item.active )
+                    {
+                        item.active = false;
+                        MelonLogger.Msg("Set MotionBlur Object To False");
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+
+        }
+    }
+}
