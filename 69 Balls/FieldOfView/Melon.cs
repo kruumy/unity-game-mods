343f85b7fe8aa2bb7288a74c4d83acd1b3b1969c
kruumy <joshuampauls@gmail.com>
2023-07-10 13:20:41 -0400

    added new game

---
+++ 69 Balls/FieldOfView/Melon.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+namespace FieldOfView
+{
+    public class Melon : MelonMod
+    {
+        [HarmonyPatch(typeof(Camera), "fieldOfView", MethodType.Setter)]
+        private static class Patch
+        {
+            private static void Prefix( ref float value )
+            {
+                value *= 1.25f;
+            }
+        }
+    }
+}
