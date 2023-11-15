7ded0b707d47604b7b1679967eeafa8e232a102f
kruumy <joshuampauls@gmail.com>
2023-11-15 17:25:39 -0500

    added lethalcompany mods

---
+++ LethalCompany/AlwaysScanning/Main.cs
@@ -0,0 +1,15 @@
+﻿namespace AlwaysScanning
+{
+    public class Main : MelonLoader.MelonMod
+    {
+        public override void OnSceneWasLoaded( int buildIndex, string sceneName )
+        {
+            if ( HUDManager.Instance != null )
+            {
+                System.Reflection.FieldInfo playerPingingScan = typeof(HUDManager).GetField("playerPingingScan", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                playerPingingScan.SetValue(HUDManager.Instance, float.MaxValue);
+                LoggerInstance.Msg("Set field playerPingingScan to max value");
+            }
+        }
+    }
+}
---
+++ LethalCompany/CustomFOV/Main.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace CustomFOV
+{
+    public class Main : MelonLoader.MelonMod
+    {
+        public override void OnSceneWasLoaded( int buildIndex, string sceneName )
+        {
+            foreach ( Object cameraobj in UnityEngine.Object.FindObjectsOfTypeAll(typeof(UnityEngine.Camera)) )
+            {
+                if ( cameraobj is UnityEngine.Camera camera && camera.name == "MainCamera" )
+                {
+                    Transform HUDHelmetPosition = camera.gameObject.transform.GetChild(0);
+                    HUDHelmetPosition.localPosition = new Vector3(0.01f, -0.048f, -0.119f);
+                }
+            }
+        }
+        [HarmonyLib.HarmonyPatch(typeof(UnityEngine.Camera), nameof(UnityEngine.Camera.fieldOfView), HarmonyLib.MethodType.Setter)]
+        private class CamerafieldOfViewPatch
+        {
+            [HarmonyLib.HarmonyPrefix]
+            private static void Prefix( ref float value )
+            {
+                value = 90;
+            }
+        }
+    }
+}
