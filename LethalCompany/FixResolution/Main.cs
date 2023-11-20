1ff48ac9eb9ab11ef4c30ce8812f26e90ec0e71b
kruumy <joshuampauls@gmail.com>
2023-11-19 20:17:42 -0500

    Created fixresolution mod for lethal company

---
+++ LethalCompany/FixResolution/Main.cs
@@ -0,0 +1,64 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using MelonLoader;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FixResolution
+{
+    public class Main : MelonMod
+    {
+        private static MelonLogger.Instance Logger = null;
+        public override void OnLateInitializeMelon()
+        {
+            Logger = LoggerInstance;
+        }
+        public override void OnSceneWasLoaded( int buildIndex, string sceneName )
+        {
+            foreach ( UnityEngine.Object cameraobj in UnityEngine.Object.FindObjectsOfTypeAll(typeof(UnityEngine.Camera)) )
+            {
+                if ( cameraobj is UnityEngine.Camera camera && camera.name == "MainCamera" )
+                {
+                    camera.targetTexture = camera.targetTexture;
+                }
+            }
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Camera), nameof(Camera.targetTexture), HarmonyLib.MethodType.Setter)]
+        class CameraTexturePatch
+        {
+            static void Prefix( UnityEngine.Camera __instance, ref UnityEngine.RenderTexture __0 )
+            {
+                if ( __0 != null && (__0.width != Screen.width || __0.height != Screen.height) )
+                {
+                    __instance.targetTexture = null;
+                    __0.Release();
+                    __0.width = Screen.width;
+                    __0.height = Screen.height;
+                    __0.Create();
+                    Logger.Msg($"Set {__0.name} to {Screen.width}x{Screen.height}");
+                }
+            }
+        }
+
+
+        [HarmonyPatch(typeof(HUDManager), "UpdateScanNodes", new System.Type[] { typeof(PlayerControllerB) })]
+        class HUDManagerScanNodesPatch
+        {
+            static void Postfix( HUDManager __instance, PlayerControllerB playerScript )
+            {
+                var scanNodesField = typeof(HUDManager).GetField("scanNodes", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                var scanNodes = (Dictionary<RectTransform, ScanNodeProperties>)scanNodesField.GetValue(__instance);
+
+                foreach ( RectTransform scan in __instance.scanElements )
+                {
+                    if ( scanNodes.TryGetValue(scan, out ScanNodeProperties scanNodeProperties) )
+                    {
+                        Vector3 vector = playerScript.gameplayCamera.WorldToScreenPoint(scanNodeProperties.transform.position);
+                        scan.anchoredPosition = new Vector2((vector.x - (Screen.width / 2)) / 2, (vector.y - (Screen.height / 2)) / 2);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LethalCompany/FixResolution/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FixResolution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FixResolution")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: MelonInfo(typeof(FixResolution.Main), nameof(FixResolution), "1.0.0", "kruumy")]
+[assembly: MelonGame("ZeekerssRBLX", "Lethal Company")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e2208386-f58f-4318-bb88-ae3ecb7247f4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
