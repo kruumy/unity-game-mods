377a3bf63d2c92aa9c59edc008da9a5543ab16da
kruumy <joshuampauls@gmail.com>
2023-12-28 18:55:59 -0500

    added superkiwi64 mods

---
+++ SuperKiwi64/FOVModifier/Main.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace FOVModifier
+{
+    [BepInPlugin("kruumy.FOVModifier", "FOVModifier", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        public void LateUpdate()
+        {
+            if ( Input.GetKeyDown(KeyCode.Minus) )
+            {
+                Camera.main.fieldOfView -= 5;
+            }
+            else if ( Input.GetKeyDown(KeyCode.Equals) )
+            {
+                Camera.main.fieldOfView += 5;
+            }
+        }
+    }
+}
---
+++ SuperKiwi64/Modernizer/Main.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Modernizer
+{
+    [BepInPlugin("kruumy.Modernizer", "Modernizer", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            SceneManager.activeSceneChanged += ( Scene arg0, Scene arg1 ) =>
+            {
+                Application.targetFrameRate = -1;
+                if
+                (
+                Camera.main != null &&
+                Camera.main.targetTexture != null &&
+                Camera.main.targetTexture.width != Screen.width
+                && Camera.main.targetTexture.height != Screen.height
+                )
+                {
+                    ChangeCameraTextureResolution(Camera.main, Screen.width, Screen.height);
+                }
+
+            };
+        }
+
+        public void ChangeCameraTextureResolution( Camera camera, int width, int height )
+        {
+            RenderTexture texture = camera.targetTexture;
+            camera.targetTexture = null;
+
+            texture.Release();
+            texture.width = width;
+            texture.height = height;
+            texture.Create();
+
+            camera.targetTexture = texture;
+        }
+    }
+}
