254aee4d4d3b926e99bc76da81c7a2aed626fc95
kruumy <joshuampauls@gmail.com>
2024-04-29 21:00:20 -0400

    Added hide double cash mod for btd6

---
+++ BloonsTD6/HideDoubleCash/Main.cs
@@ -0,0 +1,20 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(HideDoubleCash.Main), "HideDoubleCash", "1.0.0", "kruumy")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace HideDoubleCash
+{
+    public class Main : MelonMod
+    {
+        public override void OnSceneWasLoaded( int buildIndex, string sceneName )
+        {
+            GameObject TextUIElement = GameObject.Find("InGame/UIRect/MainHudLeftAlign(Clone)/LeftGroup/LayoutGroup/CashGroup/DoubleCashMode/Txt");
+            if( TextUIElement != null )
+            {
+                TextUIElement.active = false;
+            }
+        }
+    }
+}
