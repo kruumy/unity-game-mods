b8a768abaf319ce1375174c79f2a4dd31c8102c6
kruumy <joshuampauls@gmail.com>
2024-05-07 22:05:01 -0400

    Added StartingItems mod to RoR2

---
+++ RiskofRain2/StartingItems/Main.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+
+namespace StartingItems
+{
+    [BepInPlugin("kruumy.StartingItems", "StartingItems", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        public ConfigEntry<string> StartingItems;
+        public void Awake()
+        {
+            StartingItems = Config.Bind<string>(
+                "General",
+                nameof(StartingItems),
+                "JumpBoost,Hoof",
+                "A csv (comma seperated values) of all the items to give to a player on spawn. The diplay name might not always equal the codename of the item. For example: Wax Quail = JumpBoost. To find the name out for yourself, open the console (ctrl + alt + grave (`)) and type in \"item_list\". To give muliple of an item simply just repeat it. (Hoof,Hoof,Hoof)");
+            RoR2.PlayerCharacterMasterController.onPlayerAdded += PlayerCharacterMasterController_onPlayerAdded;
+        }
+
+        private void PlayerCharacterMasterController_onPlayerAdded( RoR2.PlayerCharacterMasterController player )
+        {
+            player.master.onBodyStart += Master_onBodyStart;
+            void Master_onBodyStart( RoR2.CharacterBody body )
+            {
+                foreach ( var item in StartingItems.Value.Split(',') )
+                {
+                    body.inventory.GiveItemString(item.Trim());
+                }
+                player.master.onBodyStart -= Master_onBodyStart;
+            }
+        }
+
+    }
+}
