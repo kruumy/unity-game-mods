4e98dffd2fd3447580943a9719ddec3e045dc15c
kruumy <joshuampauls@gmail.com>
2024-05-16 02:11:56 -0400

    added RoboBeat MouseAimAssist

---
+++ RoboBeat/MouseAimAssist/Main.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Harmony;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseAimAssist
+{
+    [BepInPlugin("kruumy.MouseAimAssist", "MouseAimAssist", "1.0.0")]
+    public class Main : BepInEx.BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Harmony harmony = new Harmony("kruumy.MouseAimAssist");
+            harmony.PatchAll(typeof(Patches));
+        }
+    }
+}
---
+++ RoboBeat/MouseAimAssist/Patches.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MouseAimAssist
+{
+    public class Patches
+    {
+        [HarmonyLib.HarmonyPatch(typeof(MouseLook), "UpdateAimAssist")]
+        [HarmonyLib.HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> UpdateAimAssistPrefix( IEnumerable<CodeInstruction> instructions )
+        {
+            bool startReturning = false;
+            foreach ( var instruction in instructions )
+            {
+                if ( !startReturning && instruction.opcode == OpCodes.Ldarg_0 )
+                {
+                    startReturning = true;
+                }
+                if(startReturning)
+                {
+                    yield return instruction;
+                }
+            }
+        }
+    }
+}
