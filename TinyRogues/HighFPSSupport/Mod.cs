13bfd79c2b7d9349daa12a636150c4bc1bc4b94f
kruumy <joshuampauls@gmail.com>
2025-03-11 22:16:31 -0400

    added tinyrogues HighFPSSupport

---
+++ TinyRogues/HighFPSSupport/Mod.cs
@@ -0,0 +1,16 @@
+﻿using HighFPSSupport;
+using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(Mod), nameof(HighFPSSupport), "1.0.0", "kruumy")]
+[assembly: MelonGame("RubyDev", "Tiny Rogues")]
+namespace HighFPSSupport
+{
+    public class Mod : MelonMod
+    {
+        public override void OnLateUpdate()
+        {
+            Time.fixedDeltaTime = Time.deltaTime;
+        }
+    }
+}
