9d6a37b4914ebbbdf0249f894ff612305906612b
kruumy <joshuampauls@gmail.com>
2025-03-12 00:08:49 -0400

    added BlackBackground to TinyRogues

---
+++ TinyRogues/BlackBackground/Mod.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+using UnityEngine;
+[assembly: MelonInfo(typeof(BlackBackground.Mod), nameof(BlackBackground), "1.0.0", "kruumy")]
+[assembly: MelonGame("RubyDev", "Tiny Rogues")]
+namespace BlackBackground
+{
+    public class Mod : MelonMod
+    {
+        public override void OnSceneWasInitialized( int buildIndex, string sceneName )
+        {
+            if ( Camera.main != null )
+            {
+                Camera.main.backgroundColor = Color.black;
+            }
+        }
+    }
+}
