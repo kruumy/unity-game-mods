292dde030a4cb67d3d0c70dc1fa9979d7c33b560
kruumy <joshuampauls@gmail.com>
2025-07-15 23:47:19 -0400

    Added ATLYSS mod

---
+++ ATLYSS/BetterSensitivitySlider/Main.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Linq;
+using UnityEngine.UI;
+
+namespace BetterSensitivitySlider
+{
+    [BepInPlugin("BetterSensitivitySlider.kruumy", "BetterSensitivitySlider", "1.0.0")]
+    public class Main : BepInEx.BaseUnityPlugin
+    {
+        private Harmony _harmony;
+        public void Awake()
+        {
+            _harmony = new Harmony("BetterSensitivitySlider.kruumy");
+            _harmony.PatchAll();
+        }
+    }
+}
---
+++ ATLYSS/BetterSensitivitySlider/Patches.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using UnityEngine.UI;
+
+namespace BetterSensitivitySlider
+{
+    public static class Patches
+    {
+
+        internal static void ApplySensitivitySlider( SettingsManager settingsManager )
+        {
+            var sliderField = typeof(SettingsManager).GetField("_cameraSensitivitySlider", BindingFlags.NonPublic | BindingFlags.Instance);
+            var slider = sliderField.GetValue(settingsManager) as UnityEngine.UI.Slider;
+            slider.minValue = 0f;
+        }
+
+        [HarmonyPatch(typeof(SettingsManager), "Handle_InputParameters")]
+        public static class SettingsManager_Handle_InputParameters_Patch
+        {
+            public static void Prefix( SettingsManager __instance )
+            {
+                ApplySensitivitySlider(__instance);
+            }
+        }
+
+        [HarmonyPatch(typeof(SettingsManager), "Load_SettingsData")]
+        public static class SettingsManager_Load_SettingsData_Patch
+        {
+            public static void Prefix( SettingsManager __instance )
+            {
+                ApplySensitivitySlider(__instance);
+            }
+        }
+    }
+}
