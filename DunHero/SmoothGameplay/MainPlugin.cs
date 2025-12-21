30c2ffbb98e3801c3f2b7a915734447d2c09abe3
kruumy <joshuampauls@gmail.com>
2025-12-21 10:26:00 -0500

    add dunhero smoothggameplay

---
+++ DunHero/SmoothGameplay/MainPlugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SmoothGameplay
+{
+    [BepInPlugin("com.kruumy.smoothgameplay", "SmoothGameplay", "1.0.0")]
+    public class MainPlugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            new Harmony("com.kruumy.smoothgameplay").PatchAll();
+            Logger.LogInfo("SmoothGameplay loaded");
+        }
+
+        [HarmonyPatch(typeof(Rigidbody2D), nameof(Rigidbody2D.position), MethodType.Getter)]
+        class Rigidbody2D_GetPosition_Patch
+        {
+            public static void Postfix( Rigidbody2D __instance )
+            {
+                if ( __instance.interpolation != RigidbodyInterpolation2D.Interpolate )
+                {
+                    __instance.interpolation = RigidbodyInterpolation2D.Interpolate;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Unity.Cinemachine.CinemachineBrain), "Start", MethodType.Normal)]
+        class CinemachineBrain_Start_Patch
+        {
+            public static void Postfix( Unity.Cinemachine.CinemachineBrain __instance )
+            {
+                __instance.BlendUpdateMethod = Unity.Cinemachine.CinemachineBrain.BrainUpdateMethods.LateUpdate;
+                __instance.UpdateMethod = Unity.Cinemachine.CinemachineBrain.UpdateMethods.LateUpdate;
+            }
+        }
+    }
+}
---
+++ DunHero/SmoothGameplay/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SmoothGameplay")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SmoothGameplay")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fcdb0336-fd9d-46b6-bd62-7016bf7a6813")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
