fcf870ba4411461a4e0c607ed532ac185b8ff3dc
kruumy <joshuampauls@gmail.com>
2026-02-19 03:17:45 -0500

    add Lethal League Blaze Ultrawide Fix

---
+++ Lethal League Blaze/UltraWideFix/UltraWideFix.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+
+namespace UltraWideFix
+{
+    [BepInPlugin("kruumy.plugins.llb.ultrawidefix", "UltraWideFix", "1.0.0")]
+    [BepInProcess("LLBlaze.exe")]
+    public class UltraWideFix : BaseUnityPlugin
+    {
+        public readonly float aspect = Screen.width / Screen.height;
+        public readonly Rect pixelRect = new Rect(0, 0, Screen.width, Screen.height);
+        public void LateUpdate()
+        {
+            foreach (var camera in UnityEngine.Camera.allCameras)
+            {
+                camera.aspect = aspect;
+                camera.pixelRect = pixelRect;
+            }
+        }
+    }
+}
